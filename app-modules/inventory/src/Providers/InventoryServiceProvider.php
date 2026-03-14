8c9a015624bb6f29fb1fa1be71d0c53306211b76
LAHAN Happiness <happinesstresor3@gmail.com>
2026-03-14 09:40:54 +0100

    create inventory module

---
+++ app-modules/inventory/src/Providers/InventoryServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Lahatre\Inventory\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class InventoryServiceProvider extends ServiceProvider
+{
+	public function register(): void
+	{
+	}
+	
+	public function boot(): void
+	{
+	}
+}
---
+++ app-modules/inventory/tests/Feature/Providers/InventoryServiceProviderTest.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Lahatre\Inventory\Tests;
+
+use Tests\TestCase;
+class InventoryServiceProviderTest extends TestCase
+{
+	// TODO
+}
